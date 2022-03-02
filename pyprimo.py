a875ddb6e1c08c08093fc934b327a88224290b2b
rubensalexsander <rubensalexsander0001@gmail.com>
2022-03-01 22:02:18 -0300

    commit 01-03-2022

--- pyprimo.py
+++ pyprimo.py
@@ -5,18 +5,16 @@ class Pyprimo:
         pass
 
     def isPrimo(self, num):
-        bits = int(len(str(num))) *8
+        bits = len(str(num)) *8
         for c in range(2, int(num/bits)):
             if num % c == 0:
                 return False
         if not num < 2: return True
     
     def find_primos(self, first_num, last_num):
-        listPrimos = []
-        for nowNumber in range(first_num, last_num+1):
-            if self.isPrimo(nowNumber): 
-                listPrimos.append(nowNumber)
-        return listPrimos
+        return [
+            nowNumber for nowNumber in range(first_num, last_num+1) if self.isPrimo(nowNumber)
+            ]
 
 if __name__ == '__main__':
     py_primos = Pyprimo()
@@ -29,5 +27,5 @@ if __name__ == '__main__':
 
     primos = py_primos.find_primos(first_num, last_num)
 
-    print('Finalizado,', len(primos), f'números encontrados.')
+    print('Finalizado,', len(primos), 'números encontrados.')
     print(f'Tempo (s): {str(round((time()-timenow), 4))}\n')
