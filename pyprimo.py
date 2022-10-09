df3fca93eba807092e0c068c46c042907e0ae3b7
root <root@pc1.pc1.com>
2022-10-08 22:53:38 -0300

    commit 08-10-2022

--- pyprimo.py
+++ pyprimo.py
@@ -1,31 +1,25 @@
 from time import time
 
-class Pyprimo:
-    def __init__(self):
-        pass
+def isPrimo(num):
+    bits = len(str(num))*8
+    for c in range(2, int(num/bits)):
+        if num % c == 0:
+            return False
+    if not num < 2: return True
 
-    def isPrimo(self, num):
-        bits = len(str(num)) *8
-        for c in range(2, int(num/bits)):
-            if num % c == 0:
-                return False
-        if not num < 2: return True
-    
-    def find_primos(self, first_num, last_num):
-        return [
-            nowNumber for nowNumber in range(first_num, last_num+1) if self.isPrimo(nowNumber)
-            ]
+def find_primos(first_num, last_num):
+    return [
+        nowNumber for nowNumber in range(first_num, last_num+1) if isPrimo(nowNumber)
+        ]
 
 if __name__ == '__main__':
-    py_primos = Pyprimo()
-
     first_num = 0 #Número inicial
-    last_num = 100000 #Número final
+    last_num = 200000 #Número final
 
     print('\nIniciado')
     timenow = time()
 
-    primos = py_primos.find_primos(first_num, last_num)
+    primos = find_primos(first_num, last_num)
 
     print('Finalizado,', len(primos), 'números encontrados.')
-    print(f'Tempo (s): {str(round((time()-timenow), 4))}\n')
+    print(f'Tempo (s): {round((time()-timenow), 4)}\n')
